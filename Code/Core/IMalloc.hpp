3dd8b515aef50218efeced264a7e8edd6ebc3450
Илья Игошин <igilya2003@gmail.com>
2020-01-21 22:36:52 +0500

    core: add std malloc implementation

--- Code/Core/IMalloc.hpp
+++ Code/Core/IMalloc.hpp
@@ -6,11 +6,16 @@ namespace ray::core::memory
 	class IMalloc
 	{
 	public:
+		IMalloc() : m_AllocatedMemorySize(0) {}
 		virtual ~IMalloc();
 
-		virtual void* Alloc(size_t Size, size_t Align = 0) = 0;
+		virtual void* Alloc(size_t, size_t) = 0;
 		virtual void Free(void*) = 0;
 		virtual void* Realloc(void*, size_t) = 0;
+		inline size_t GetAllocatedMemorySize() { return m_AllocatedMemorySize; }
+
+	protected:
+		size_t m_AllocatedMemorySize;
 
 	};
 
---
+++ Code/Core/StdMalloc.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "IMalloc.hpp"
+
+namespace ray::core::memory
+{
+
+	class StdMalloc 
+		: public IMalloc
+	{
+	public:
+		~StdMalloc() override {}
+
+		void* Alloc(size_t Size, size_t Alignment = 0) override;
+		void Free(void*);
+		void* Realloc(void*, size_t) override;
+
+	};
+
+}
+
+
+
