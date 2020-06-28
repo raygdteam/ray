e56ae6f8bff11a9517d3b6f8708c5300bf882b7b
Илья Игошин <igilya2003@gmail.com>
2020-06-28 23:20:23 +0500

    Renderer Core API: math: vectors has been added

---
+++ source/engine/modules/renderer_core/math/vectors.cpp
@@ -0,0 +1,6 @@
+#include "vectors.hpp"
+
+namespace ray::renderer_core_api::math
+{
+
+}
--- source/engine/modules/renderer_core/renderer_commands.hpp
+++ source/engine/modules/renderer_core/renderer_commands.hpp
@@ -1,19 +1,23 @@
 #pragma once
 #include "renderer.hpp"
+#include "math/vectors.hpp"
 #include <core/core.hpp>
 
 namespace ray::renderer_core_api
 {
 	struct Vertex
 	{
-		float position[3];
+		math::Vector3 _position;
+		math::Vector4 _color;
 	};
 
 	enum class PrimitiveTopology
 	{
+		undefined, //can't draw anything
 		point, 
 		line, 
-		triangle
+		triangle,
+		patch //for tesselation
 	};
 
 	class RendererCommands
@@ -21,7 +25,7 @@ namespace ray::renderer_core_api
 	public:
 		static bool Initialize(IRenderer* renderer);
 
-		static void Draw(Vertex* vertices, PrimitiveTopology topology);
+		static void Draw(Vertex* vertices, PrimitiveTopology topology = PrimitiveTopology::undefined);
 		static void DrawIndexed(Vertex* vertices, u32* indices);
 
 	};
