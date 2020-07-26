ced6fdb291952da4529c2a60246a7a93e45d1230
darkhpp <sevastyandeveloper@gmail.com>
2020-07-26 14:05:01 +0700

    core: Refactor Array<>

--- source/engine/modules/core/lib/array.hpp
+++ source/engine/modules/core/lib/array.hpp
@@ -1,129 +1,130 @@
 #pragma once
+#include <core/core.hpp>
 
 namespace ray
 {
-	template<typename type>
-	class Array
+template<typename type, typename IndexSize = u32>
+class Array
+{
+	IndexSize _size;
+	Node<type>* _head;
+
+public:
+	Array()
 	{
-	public:
-		Array()
+		_size = 0;
+		this->_head = nullptr;
+	}
+
+	~Array()
+	{
+		Clear();
+	}
+
+	// void Push(type data, int index);
+
+	void PushFront(type data)
+	{
+		this->_head = new Node<type>(data, this->_head);
+		_size++;
+	}
+
+	void PushBack(type data)
+	{
+		if (this->_head == nullptr)
+			this->_head = new Node<type>(data);
+		else
 		{
-			_size = 0;
-			this->_head = nullptr;
+			Node<type>* current = this->_head;
+
+			while (current->_next != nullptr)
+				current = current->_next;
+
+			current->_next = new Node<type>(data);
 		}
 
-		~Array()
+		_size++;
+	}
+
+	// void Insert(type data, int index);
+
+	// void InsertFront(type data);
+	// void InsertBack(type data);
+
+	void Pop(IndexSize index)
+	{
+		if (index == 0)
+			PopFront();
+		else
 		{
-			Clear();
-		}
+			Node<type>* previous = this->_head;
+			for (IndexSize i = 0; i < index - 1; i++)
+				previous = previous->_next;
 
-		// void Push(type data, int index);
+			Node<type>* toDelete = previous->_next;
 
-		void PushFront(type data)
-		{
-			this->_head = new Node<type>(data, this->_head);
-			_size++;
-		}
+			previous->_next = toDelete->_next;
 
-		void PushBack(type data)
-		{
-			if (this->_head == nullptr)
-				this->_head = new Node<type>(data);
-			else
-			{
-				Node<type>* current = this->_head;
-
-				while (current->_nextPointer != nullptr)
-					current = current->_nextPointer;
-
-				current->_nextPointer = new Node<type>(data);
-			}
-
-			_size++;
-		}
-
-		// void Insert(type data, int index);
-
-		// void InsertFront(type data);
-		// void InsertBack(type data);
-
-		void Pop(int index)
-		{
-			if (index == 0)
-				PopFront();
-			else
-			{
-				Node<type>* previous = this->_head;
-				for (int i = 0; i < index - 1; i++)
-					previous = previous->_nextPointer;
-
-				Node<type>* toDelete = previous->_nextPointer;
-
-				previous->_nextPointer = toDelete->_nextPointer;
-
-				delete toDelete;
-
-				_size--;
-			}
-		}
-
-		void PopFront()
-		{
-			Node<type>* temp = this->_head;
-
-			this->_head = this->_head->_nextPointer;
-
-			delete temp;
+			delete toDelete;
 
 			_size--;
 		}
+	}
 
-		void PopBack()
+	void PopFront()
+	{
+		Node<type>* temp = this->_head;
+
+		this->_head = this->_head->_next;
+
+		delete temp;
+
+		_size--;
+	}
+
+	void PopBack()
+	{
+		Pop(_size - 1);
+	}
+
+	void Clear()
+	{
+		while (this->_size)
+			PopFront();
+	}
+
+	IndexSize Size() { return this->_size; }
+	bool IsEmpty() { return this->_size == 0; }
+
+	type& operator[](const int index)
+	{
+		IndexSize counter = 0;
+
+		Node<type>* current = this->_head;
+
+		while (current != nullptr)
 		{
-			Pop(_size - 1);
+			if (counter == index)
+				return current->_data;
+
+			current = current->_next;
+			counter++;
 		}
+	}
 
-		void Clear()
+private:
+	template<typename type>
+	class Node
+	{
+	public:
+		Node* _next;
+		type _data;
+
+		Node(type data = type(), Node* next = nullptr)
 		{
-			while (this->_size)
-				PopFront();
+			this->_data = data;
+			this->_next = next;
 		}
-
-		int Size() { return this->_size; }
-		bool IsEmpty() { return this->_size == 0; }
-
-		type& operator[](const int index)
-		{
-			int counter = 0;
-
-			Node<type>* current = this->_head;
-
-			while (current != nullptr)
-			{
-				if (counter == index)
-					return current->_data;
-
-				current = current->_nextPointer;
-				counter++;
-			}
-		}
-
-	private:
-		template<typename type>
-		class Node
-		{
-		public:
-			Node* _nextPointer;
-			type _data;
-
-			Node(type data = type(), Node* _nextPointer = nullptr)
-			{
-				this->_data = data;
-				this->_nextPointer = _nextPointer;
-			}
-		};
-
-		int _size;
-		Node<type>* _head;
 	};
+};
 }
