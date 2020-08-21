02888639c07ba9c1181676df59f1c144db174589
Илья Игошин <igilya2003@gmail.com>
2020-08-21 23:14:05 +0500

    renderer_core: root signature

--- source/engine/modules/renderer_core/root_signature.cpp
+++ source/engine/modules/renderer_core/root_signature.cpp
@@ -1,1 +1,22 @@
 #include "root_signature.hpp"
+#include "renderer.hpp"
+#include "d3dx12.h"
+#include <core/debug/assert.hpp>
+
+namespace ray::renderer_core_api
+{
+	void RootSignature::Finalize(D3D12_ROOT_SIGNATURE_FLAGS flags)
+	{
+		// temp solution
+
+		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
+		rootSignatureDesc.Init(0, nullptr, 0, nullptr, flags);
+
+		ID3DBlob* signature;
+		auto hr = D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, nullptr);
+		check(hr == S_OK)
+
+		hr = globals::gDevice->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&_rootSignature));
+		check(hr == S_OK)
+	}
+}
--- source/engine/modules/renderer_core/root_signature.hpp
+++ source/engine/modules/renderer_core/root_signature.hpp
@@ -7,6 +7,7 @@ namespace ray::renderer_core_api
 	class RootSignature
 	{
 	public:
+		void Finalize(D3D12_ROOT_SIGNATURE_FLAGS flags = D3D12_ROOT_SIGNATURE_FLAG_NONE);
 		ID3D12RootSignature* GetRootSignature() const noexcept { return _rootSignature; }
 
 	private:
