d8fa0a7ee1c11bc8e22b17d025b259eb796f8e08
darkhpp <sevastyandeveloper@gmail.com>
2020-08-23 16:43:14 +0300

    renderer: impement RootSignatureParameters

--- source/engine/modules/renderer_core/root_signature.cpp
+++ source/engine/modules/renderer_core/root_signature.cpp
@@ -5,14 +5,35 @@
 
 namespace ray::renderer_core_api
 {
+	RootSignatureParameter::RootSignatureParameter()
+	{
+		_parameter = {};
+	}
+
+	void RootSignature::Begin(u32 numParams, u32 numStaticSamplers)
+	{
+		check(_rootParameters.IsEmpty());
+
+		for (u32 i = 0; i < numParams; ++i)
+		{
+			_rootParameters.PushBack(new RootSignatureParameter());
+		}
+	}
+
 	void RootSignature::Finalize(D3D12_ROOT_SIGNATURE_FLAGS flags)
 	{
-		// temp solution
+		/*
+		 * It is safe to reinterpret_cast<> since that class has no vtable, nor has any virtual functions.
+		 * The D3D12_ROOT_PARAMETER comes first in class structure.
+		 */
+		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc = {};
+		rootSignatureDesc.NumParameters = u32(_rootParameters.Size());
+		rootSignatureDesc.pParameters = reinterpret_cast<D3D12_ROOT_PARAMETER*>(_rootParameters.data());
+		rootSignatureDesc.NumStaticSamplers = 0;
+		rootSignatureDesc.pStaticSamplers = nullptr;
+		rootSignatureDesc.Flags = flags;
 
-		CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc;
-		rootSignatureDesc.Init(0, nullptr, 0, nullptr, flags);
-
-		ID3DBlob* signature;
+		ID3DBlob* signature = nullptr;
 		auto hr = D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, nullptr);
 		check(hr == S_OK)
 
