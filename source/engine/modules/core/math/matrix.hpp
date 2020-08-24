16d3b068dd4387b9b85d2bb5d17474cbeb08bfe9
darkhpp <sevastyandeveloper@gmail.com>
2020-08-24 14:59:28 +0300

    core/matrix: Matrix4 Scale

--- source/engine/modules/core/math/matrix.hpp
+++ source/engine/modules/core/math/matrix.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <core/math/vector.hpp>
+#include <initializer_list>
+#include "core/debug/assert.hpp"
 
 // 'F' stands for 'fast'
 template<u8 M_Row, u8 N_Column>
@@ -10,23 +12,29 @@ struct FMatrix<2, 2>
 {
 	FVector<2> Row[2];
 
+	FMatrix(std::initializer_list<FVector<2>> list)
+	{
+		check(list.size() <= 2);
+		
+		for (u8 i = 0; i < list.size(); ++i)
+		{
+			Row[i] = list.begin()[i];
+		}
+	}
+
 	FMatrix<2, 2> Add(const FMatrix<2, 2>& param)
 	{
 		return FMatrix<2, 2> {
-			.Row = {
-				FVector<2> {.x = Row[0].x + param.Row[0].x, .y = Row[0].y + param.Row[0].y },
-				FVector<2> {.x = Row[1].x + param.Row[1].x, .y = Row[1].y + param.Row[1].y },
-			}
+			FVector<2> {.x = Row[0].x + param.Row[0].x, .y = Row[0].y + param.Row[0].y },
+			FVector<2> {.x = Row[1].x + param.Row[1].x, .y = Row[1].y + param.Row[1].y },
 		};
 	}
 
 	FMatrix<2, 2> Substract(const FMatrix<2, 2>& param)
 	{
 		return FMatrix<2, 2> {
-			.Row = {
-				FVector<2> {.x = Row[0].x - param.Row[0].x, .y = Row[0].y - param.Row[0].y },
-				FVector<2> {.x = Row[1].x - param.Row[1].x, .y = Row[1].y - param.Row[1].y },
-			}
+			FVector<2> {.x = Row[0].x - param.Row[0].x, .y = Row[0].y - param.Row[0].y },
+			FVector<2> {.x = Row[1].x - param.Row[1].x, .y = Row[1].y - param.Row[1].y },
 		};
 	}
 
@@ -45,26 +53,32 @@ template<>
 struct FMatrix<3, 3>
 {
 	FVector<3> Row[3];
+
+	FMatrix(std::initializer_list<FVector<3>> list)
+	{
+		check(list.size() <= 3);
+		
+		for (u8 i = 0; i < list.size(); ++i)
+		{
+			Row[i] = list.begin()[i];
+		}
+	}
 	
 	FMatrix<3, 3> Add(const FMatrix<3, 3>& param)
 	{
 		return FMatrix<3, 3> {
-			.Row = {
-				FVector<3> {.x = Row[0].x + param.Row[0].x, .y = Row[0].y + param.Row[0].y, .z = Row[0].z + param.Row[0].z },
-				FVector<3> {.x = Row[1].x + param.Row[1].x, .y = Row[1].y + param.Row[1].y, .z = Row[1].z + param.Row[1].z },
-				FVector<3> {.x = Row[2].x + param.Row[2].x, .y = Row[2].y + param.Row[2].y, .z = Row[2].z + param.Row[2].z },
-			}
+			FVector<3> {.x = Row[0].x + param.Row[0].x, .y = Row[0].y + param.Row[0].y, .z = Row[0].z + param.Row[0].z },
+			FVector<3> {.x = Row[1].x + param.Row[1].x, .y = Row[1].y + param.Row[1].y, .z = Row[1].z + param.Row[1].z },
+			FVector<3> {.x = Row[2].x + param.Row[2].x, .y = Row[2].y + param.Row[2].y, .z = Row[2].z + param.Row[2].z },
 		};
 	}
 
 	FMatrix<3, 3> Substract(const FMatrix<3, 3>& param)
 	{
 		return FMatrix<3, 3> {
-			.Row = {
-				FVector<3> {.x = Row[0].x - param.Row[0].x, .y = Row[0].y - param.Row[0].y, .z = Row[0].z - param.Row[0].z },
-				FVector<3> {.x = Row[1].x - param.Row[1].x, .y = Row[1].y - param.Row[1].y, .z = Row[1].z - param.Row[1].z },
-				FVector<3> {.x = Row[2].x - param.Row[2].x, .y = Row[2].y - param.Row[2].y, .z = Row[2].z - param.Row[2].z },
-			}
+			FVector<3> {.x = Row[0].x - param.Row[0].x, .y = Row[0].y - param.Row[0].y, .z = Row[0].z - param.Row[0].z },
+			FVector<3> {.x = Row[1].x - param.Row[1].x, .y = Row[1].y - param.Row[1].y, .z = Row[1].z - param.Row[1].z },
+			FVector<3> {.x = Row[2].x - param.Row[2].x, .y = Row[2].y - param.Row[2].y, .z = Row[2].z - param.Row[2].z },
 		};
 	}
 
@@ -84,27 +98,53 @@ struct FMatrix<4, 4>
 {
 	FVector<4> Row[4];
 
+	FMatrix(std::initializer_list<FVector<4>> list)
+	{
+		check(list.size() <= 4);
+		
+		for (u8 i = 0; i < list.size(); ++i)
+		{
+			Row[i] = list.begin()[i];
+		}
+	}
+
 	FMatrix<4, 4> Add(const FMatrix<4, 4>& param)
 	{
 		return FMatrix<4, 4> {
-			.Row = {
-				FVector<4> {.x = Row[0].x + param.Row[0].x, .y = Row[0].y + param.Row[0].y, .z = Row[0].z + param.Row[0].z, .w = Row[0].w + param.Row[0].w },
-				FVector<4> {.x = Row[1].x + param.Row[1].x, .y = Row[1].y + param.Row[1].y, .z = Row[1].z + param.Row[1].z, .w = Row[1].w + param.Row[1].w },
-				FVector<4> {.x = Row[2].x + param.Row[2].x, .y = Row[2].y + param.Row[2].y, .z = Row[2].z + param.Row[2].z, .w = Row[2].w + param.Row[2].w },
-				FVector<4> {.x = Row[3].x + param.Row[3].x, .y = Row[3].y + param.Row[3].y, .z = Row[3].z + param.Row[3].z, .w = Row[3].w + param.Row[3].w },
-			}
+			FVector<4> {.x = Row[0].x + param.Row[0].x, .y = Row[0].y + param.Row[0].y, .z = Row[0].z + param.Row[0].z, .w = Row[0].w + param.Row[0].w },
+			FVector<4> {.x = Row[1].x + param.Row[1].x, .y = Row[1].y + param.Row[1].y, .z = Row[1].z + param.Row[1].z, .w = Row[1].w + param.Row[1].w },
+			FVector<4> {.x = Row[2].x + param.Row[2].x, .y = Row[2].y + param.Row[2].y, .z = Row[2].z + param.Row[2].z, .w = Row[2].w + param.Row[2].w },
+			FVector<4> {.x = Row[3].x + param.Row[3].x, .y = Row[3].y + param.Row[3].y, .z = Row[3].z + param.Row[3].z, .w = Row[3].w + param.Row[3].w },
 		};
 	}
 
 	FMatrix<4, 4> Substract(const FMatrix<4, 4>& param)
 	{
 		return FMatrix<4, 4> {
-			.Row = {
-				FVector<4> {.x = Row[0].x - param.Row[0].x, .y = Row[0].y - param.Row[0].y, .z = Row[0].z - param.Row[0].z, .w = Row[0].w - param.Row[0].w },
-				FVector<4> {.x = Row[1].x - param.Row[1].x, .y = Row[1].y - param.Row[1].y, .z = Row[1].z - param.Row[1].z, .w = Row[1].w - param.Row[1].w },
-				FVector<4> {.x = Row[2].x - param.Row[2].x, .y = Row[2].y - param.Row[2].y, .z = Row[2].z - param.Row[2].z, .w = Row[2].w - param.Row[2].w },
-				FVector<4> {.x = Row[3].x - param.Row[3].x, .y = Row[3].y - param.Row[3].y, .z = Row[3].z - param.Row[3].z, .w = Row[3].w - param.Row[3].w },
-			}
+			FVector<4> {.x = Row[0].x - param.Row[0].x, .y = Row[0].y - param.Row[0].y, .z = Row[0].z - param.Row[0].z, .w = Row[0].w - param.Row[0].w },
+			FVector<4> {.x = Row[1].x - param.Row[1].x, .y = Row[1].y - param.Row[1].y, .z = Row[1].z - param.Row[1].z, .w = Row[1].w - param.Row[1].w },
+			FVector<4> {.x = Row[2].x - param.Row[2].x, .y = Row[2].y - param.Row[2].y, .z = Row[2].z - param.Row[2].z, .w = Row[2].w - param.Row[2].w },
+			FVector<4> {.x = Row[3].x - param.Row[3].x, .y = Row[3].y - param.Row[3].y, .z = Row[3].z - param.Row[3].z, .w = Row[3].w - param.Row[3].w },
+		};
+	}
+
+	static FMatrix<4, 4> Scale(f32 scalar) 
+	{
+		return FMatrix<4, 4> {
+			{ .x = scalar,	.y = 0.f,		.z = 0.f,		.w = 0.f },
+			{ .x = 0.f,		.y = scalar,	.z = 0.f,		.w = 0.f },
+			{ .x = 0.f,		.y = 0.f,		.z = scalar,	.w = 0.f },
+			{ .x = 0.f,		.y = 0.f,		.z = 0.f,		.w = 1.f },
+		};
+	}
+
+	static FMatrix<4, 4> Scale(FVector<3> scale) 
+	{
+		return FMatrix<4, 4> {
+			{ .x = scale.x, .y = 0.f, 		.z = 0.f, 		.w = 0.f },
+			{ .x = 0.f,		.y = scale.y,	.z = 0.f,		.w = 0.f },
+			{ .x = 0.f,		.y = 0.f,		.z = scale.z,	.w = 0.f },
+			{ .x = 0.f,		.y = 0.f,		.z = 0.f,		.w = 1.f },
 		};
 	}
 
