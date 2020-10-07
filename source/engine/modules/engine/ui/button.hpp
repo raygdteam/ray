2713157b21a91ab5ffa1401b8933b4d8202dc994
Ray Engine <vashlol@mail.ru>
2020-10-07 18:32:47 +0600

    я сделал кнопку (спасибо севе)

--- source/engine/modules/engine/ui/button.hpp
+++ source/engine/modules/engine/ui/button.hpp
@@ -2,7 +2,17 @@
 
 #include <renderer_core/renderer_2d.hpp>
 
-bool button(const FVector<3>& pos, const FVector<2>& size, FVector<2>* textureCoords, ray::renderer_core_api::GraphicsContext& gfxContext)
+#include <input/input.hpp>
+
+namespace ui
 {
-	ray::renderer_core_api::Renderer2D::DrawQuad(pos, size, textureCoords, gfxContext);
+	/*bool button(const FVector<3>& pos, const FVector<2>& size, FVector<2>* textureCoords, ray::renderer_core_api::GraphicsContext& gfxContext)
+	{
+		ray::renderer_core_api::Renderer2D::DrawQuad(pos, size, textureCoords, gfxContext);
+	}*/
+
+	bool button(const FVector<2>& position, const FVector<2>& size)
+	{
+		return input::mouse::x() >= position.x && input::mouse::x() <= size.x && input::mouse::y() >= position.y && input::mouse::y() <= size.y;
+	}
 }
