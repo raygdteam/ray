c7d0bda967292f3a7859e0f6deb12125f8baa04f
Ray Engine <vashlol@mail.ru>
2020-10-21 17:01:45 +0600

    audio: переработал массив с microsoft в c-lang \ фикс компиляции

--- source/engine/modules/audio/audio.cpp
+++ source/engine/modules/audio/audio.cpp
@@ -22,10 +22,10 @@ header::riff* AudioManager::Load(const char* filename)
 	auto file_handle = krnlFileOpen(filename, 0);
 	auto file_size = GetFileSize(file_handle, 0);
 
-	auto file_data = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, file_size); // is this flag correct ?
+	auto file_data = malloc(file_size);
 
 	if (krnlFileRead(file_handle, file_data, file_size) == HFILE_ERROR)
-		HeapFree(GetProcessHeap(), 0, file_data); // don't forget about file_handle and file_size!
+		free(file_data); // don't forget about file_handle and file_size!
 
 	krnlFileClose(file_handle);
 
@@ -36,11 +36,11 @@ void AudioManager::Play(header::riff* wave)
 {
 	/* in the future all this should be moved to the constructor */
 
-	CoCreateInstance(CLSID_MMDeviceEnumerator, NULL, CLSCTX_ALL, IID_IMMDeviceEnumerator, (void**)&this->DeviceEnumerator);
+	CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&this->DeviceEnumerator);
 
 	this->DeviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &this->Device);
 
-	this->Device->Activate(IID_IAudioClient, CLSCTX_ALL, NULL, (void**)&this->AudioClient);
+	this->Device->Activate(__uuidof(IAudioClient), CLSCTX_ALL, NULL, (void**)&this->AudioClient);
 
 	this->AudioClient->GetMixFormat(&this->DeviceFormat);
 
@@ -48,7 +48,7 @@ void AudioManager::Play(header::riff* wave)
 
 	// this->AudioClient->GetBufferSize(&);
 
-	this->AudioClient->GetService(IID_IAudioRenderClient, (void**)&this->AudioRenderClient);
+	this->AudioClient->GetService(__uuidof(IAudioRenderClient), (void**)&this->AudioRenderClient);
 
 	/* in the future all this should be moved to the constructor */
 }
