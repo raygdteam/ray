7092bfaf365f826a13d24a7ce77b8df6ac17cb15
Ray Engine <vashlol@mail.ru>
2020-10-27 13:54:32 +0600

    audio: последний коммит, звук работает но это далеко не идеальный вариант

--- source/engine/modules/audio/audio.cpp
+++ source/engine/modules/audio/audio.cpp
@@ -7,7 +7,17 @@ extern "C"
 
 AudioManager::AudioManager()
 {
-	CoInitialize(NULL);
+	CoInitialize(NULL); // see CoInitializeEx later.
+
+    CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (LPVOID*)(&this->DeviceEnumerator));
+
+    this->DeviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &this->EndPoint);
+
+    this->DeviceEnumerator->Release();
+
+    this->EndPoint->Activate(__uuidof(IAudioClient), CLSCTX_ALL, nullptr, (LPVOID*)(&this->AudioClient));
+
+    this->EndPoint->Release();
 }
 
 AudioManager::~AudioManager()
@@ -22,7 +32,7 @@ header::riff* AudioManager::Load(const char* filename)
 	auto file_handle = krnlFileOpen(filename, 0);
 	auto file_size = GetFileSize(file_handle, 0);
 
-	auto file_data = malloc(file_size);
+	auto file_data = malloc(file_size); // it needs an alternative.
 
 	if (krnlFileRead(file_handle, file_data, file_size) == HFILE_ERROR)
 		free(file_data); // don't forget about file_handle and file_size!
@@ -34,21 +44,53 @@ header::riff* AudioManager::Load(const char* filename)
 
 void AudioManager::Play(header::riff* wave)
 {
-	/* in the future all this should be moved to the constructor */
+    auto wave_samples = &wave->samples;
 
-	CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&this->DeviceEnumerator);
+    this->DeviceFormat.wFormatTag = WAVE_FORMAT_PCM;
+    this->DeviceFormat.nChannels = 2;
+    this->DeviceFormat.nSamplesPerSec = wave->sample_rate;
+    this->DeviceFormat.wBitsPerSample = 16;
+    this->DeviceFormat.nBlockAlign = (this->DeviceFormat.nChannels * this->DeviceFormat.wBitsPerSample) / 8;
+    this->DeviceFormat.nAvgBytesPerSec = this->DeviceFormat.nSamplesPerSec * this->DeviceFormat.nBlockAlign;
 
-	this->DeviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &this->EndPoint);
+    /*REFERENCE_TIME device_period;
+    this->AudioClient->GetDevicePeriod(&device_period, nullptr);*/
 
-	this->EndPoint->Activate(__uuidof(IAudioClient), CLSCTX_ALL, NULL, (void**)&this->AudioClient);
+    this->AudioClient->Initialize(AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_RATEADJUST | AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM | AUDCLNT_STREAMFLAGS_SRC_DEFAULT_QUALITY /* flags are needed, otherwise the function will not work. */, 10000000000 * 2, 0, &this->DeviceFormat, nullptr);
 
-	this->AudioClient->GetMixFormat(&this->DeviceFormat);
+    this->AudioClient->GetService(__uuidof(IAudioRenderClient), (LPVOID*)(&this->AudioRenderClient));
 
-	this->AudioClient->Initialize(AUDCLNT_SHAREMODE_SHARED, 0, 1000000 /* number of microseconds in a second */, 0, this->DeviceFormat, NULL);
+    UINT32 bufferSizeInFrames;
+    this->AudioClient->GetBufferSize(&bufferSizeInFrames);
 
-	// this->AudioClient->GetBufferSize(&);
+    this->AudioClient->Start();
 
-	this->AudioClient->GetService(__uuidof(IAudioRenderClient), (void**)&this->AudioRenderClient);
+    int wave_playback_sample = 0;
 
-	/* in the future all this should be moved to the constructor */
+    while (true) // sound playback will be endless
+    {
+        unsigned int buffer_padding;
+        this->AudioClient->GetCurrentPadding(&buffer_padding);
+
+        unsigned int frames2write = bufferSizeInFrames / 50 - buffer_padding;
+
+        int16_t* buffer;
+        this->AudioRenderClient->GetBuffer(frames2write, (BYTE**)(&buffer));
+
+        for (unsigned int frame_index = 0; frame_index < frames2write; ++frame_index)
+        {
+            *buffer++ = wave_samples[wave_playback_sample]; // left
+            *buffer++ = wave_samples[wave_playback_sample]; // right
+
+            ++wave_playback_sample;
+            wave_playback_sample %= wave->data_bytes / (wave->num_channels * sizeof(short));
+        }
+
+        this->AudioRenderClient->ReleaseBuffer(frames2write, 0);
+    }
+
+    this->AudioClient->Stop();
+    this->AudioClient->Release();
+
+    this->AudioRenderClient->Release();
 }
--- source/engine/modules/audio/audio.hpp
+++ source/engine/modules/audio/audio.hpp
@@ -6,10 +6,12 @@ namespace header
 {
     struct riff
     {
+        // riff chunk
         char riff_header[4];
         int wave_size;
         char wave_header[4];
 
+        // fmt chunk
         char fmt_header[3];
         int fmt_chunk_size;
         short audio_format;
@@ -19,8 +21,10 @@ namespace header
         short sample_alignment;
         short bit_depth;
 
+        // data chunk
         char data_header[4];
         int data_bytes;
+        short samples;
     };
 }
 
@@ -41,7 +45,7 @@ private:
     IMMDeviceEnumerator* DeviceEnumerator;
     IMMDevice* EndPoint;
 
-    WAVEFORMATEX* DeviceFormat; // see WAVEFORMATEXTENSIBLE later.
+    WAVEFORMATEX DeviceFormat = {}; // see WAVEFORMATEXTENSIBLE later.
 
     IAudioClient* AudioClient;
     IAudioRenderClient* AudioRenderClient;
