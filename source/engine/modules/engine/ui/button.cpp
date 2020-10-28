568883f34eac94f9c6b56def8c42ead5846cccde
Илья Игошин <igilya2003@gmail.com>
2020-10-28 23:38:22 +0500

    fixed texture coordinates

--- source/engine/modules/engine/ui/button.cpp
+++ source/engine/modules/engine/ui/button.cpp
@@ -14,13 +14,21 @@ UiButton::UiButton()
 	_transform = new Transform;
 	UiRenderData* ctx = _renderData = new UiRenderData;
 
-	ctx->Position = FVector3 { _transform->Position.x, _transform->Position.y, 1.f };
+	ctx->Position = FVector3 { 400.f, 600.f, 1.f };
 	ctx->Scale = FVector2 { 1.f, 1.f };
 
-	ctx->TexCoord.PushBack(FVector2 {-1.f, -1.f});
-	ctx->TexCoord.PushBack(FVector2 {1.f, -1.f});
-	ctx->TexCoord.PushBack(FVector2 {1.f, 1.f});
-	ctx->TexCoord.PushBack(FVector2 {-1.f, 1.f});
+	static FVector<2> textureCoords[4] =
+	{
+		{ 0.f, 1.f },
+		{ 0.f, 0.f },
+		{ 1.f, 0.f },
+		{ 1.f, 1.f }
+	};
+
+	ctx->TexCoord.PushBack(textureCoords[0]);
+	ctx->TexCoord.PushBack(textureCoords[1]);
+	ctx->TexCoord.PushBack(textureCoords[2]);
+	ctx->TexCoord.PushBack(textureCoords[3]);
 	
 	ctx->TextureIndex = 0;
 }
