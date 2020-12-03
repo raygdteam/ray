a0284086e105d7ccf204aaaa5a2e32ea4e3498a0
Илья Игошин <igilya2003@gmail.com>
2020-12-03 19:44:01 +0500

    renderer_core: graphics memory manager part 2315235

--- source/engine/modules/renderer_core/resources/graphics_memory_manager.cpp
+++ source/engine/modules/renderer_core/resources/graphics_memory_manager.cpp
@@ -4,53 +4,93 @@
 
 namespace ray::renderer_core_api::resources
 {
-	void GraphicsMemoryManager::Initialize(size_t maxTexturePoolSize, size_t maxBufferPoolSize) noexcept
-	{
-		CreateUploadBuffer();
-		CreateTexturePool(maxTexturePoolSize);
-		CreateBufferPool(maxBufferPoolSize);
-	}
 
-	void GraphicsMemoryManager::CreateUploadBuffer() noexcept
+	GpuMemoryPool& GpuMemoryManager::RequestPool(u64 requestedSize, u16 heapType, u16 heapResourcesType, u16 resourceType) noexcept
 	{
-		CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_UPLOAD);
-		auto desc = CD3DX12_RESOURCE_DESC::Buffer(MAX_UPLOAD_BUFFER_SIZE);
-		auto hr = globals::gDevice->CreateCommittedResource(&heapProps, D3D12_HEAP_FLAG_NONE, &desc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&_uploadBuffer));
+		check(heapType >= HEAP_TYPES_COUNT || heapResourcesType >= HEAP_RESOURCES_TYPES_COUNT, resourceType >= RESOURCE_TYPES_COUNT)
 		
-		check(hr == S_OK)
-		CD3DX12_RANGE range(0, 0);
-		_uploadBuffer->Map(0, &range, &_beginOfUploadBuffer);
-		_currentPointerOfUploadBuffer = static_cast<u8*>(_beginOfUploadBuffer);
-		_endOfUploadBuffer = _currentPointerOfUploadBuffer + MAX_UPLOAD_BUFFER_SIZE;
+		auto poolsCount = _pools[heapType][heapResourcesType][resourceType].Size();
+		if (poolsCount <= 0)
+			return CreateNewPool(requestedSize, heapType, heapResourcesType, resourceType);
+
+		auto& pool = _pools[heapType][heapResourcesType][resourceType].At(poolsCount - 1);
+
+		if (pool.AvailableSpace < requestedSize)
+			return CreateNewPool(requestedSize, heapType, heapResourcesType, resourceType);
+
+		return pool;
 	}
 
-	void GraphicsMemoryManager::CreateTexturePool(size_t maxTexturePoolSize) noexcept
+	GpuMemoryPool& GpuMemoryManager::CreateNewPool(u64 requestedSize, u16 heapType, u16 heapResourcesType, u16 resourceType) noexcept
 	{
+		GpuMemoryPool newPool;
 
+		if (resourceType == RESOURCE_TYPE_COMMITTED)
+			newPool.Committed.Pool = CreateCommittedPool(requestedSize, heapType, heapResourcesType);
+		else if (resourceType == RESOURCE_TYPE_PLACED)
+			newPool.Placed.Pool = CreateHeap(requestedSize, heapType, heapResourcesType);
+
+		newPool.AvailableSpace = requestedSize;
+		_pools[heapType][heapResourcesType][resourceType].PushBack(newPool);
+		auto index = _pools[heapType][heapResourcesType][resourceType].Size() - 1;
+
+		return _pools[heapType][heapResourcesType][resourceType].At(index);
 	}
 
-	void GraphicsMemoryManager::CreateBufferPool(size_t maxBufferPoolSize) noexcept
+	ID3D12Heap* GpuMemoryManager::CreateHeap(u64 requestedSize, u16 heapType, u16 heapResourcesType) noexcept
 	{
+		ID3D12Heap* heap;
 
+		D3D12_HEAP_PROPERTIES heapProps = {};
+		heapProps.CreationNodeMask = 1;
+		heapProps.VisibleNodeMask = 1;
+		heapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+		heapProps.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+		heapProps.Type = D3D12_HEAP_TYPE_DEFAULT;
+
+		D3D12_HEAP_DESC desc = {};
+		desc.Alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+		desc.SizeInBytes = requestedSize;
+		desc.Properties = heapProps;
+		desc.Flags = D3D12_HEAP_FLAG_NONE;
+
+		globals::gDevice->CreateHeap(&desc, IID_PPV_ARGS(&heap));
+
+		return heap;
 	}
 
-	void* GraphicsMemoryManager::AllocateFromUploadBuffer(size_t requestedSize) noexcept
+	ID3D12Resource* GpuMemoryManager::CreateCommittedPool(u64 requestedSize, u16 heapType, u16 heapResourcesType) noexcept
 	{
-		ray_assert((_currentPointerOfUploadBuffer + requestedSize) <= _endOfUploadBuffer, "Out of upload buffer!")
-		
-		void* ret = _currentPointerOfUploadBuffer;
-		_currentPointerOfUploadBuffer += requestedSize;
-		return ret;
+		D3D12_HEAP_PROPERTIES heapProps = {};
+		heapProps.CreationNodeMask = 1;
+		heapProps.VisibleNodeMask = 1;
+		heapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+		heapProps.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+		heapProps.Type = D3D12_HEAP_TYPE_DEFAULT;
+
+		D3D12_RESOURCE_DESC resourceDesc;
+		resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		resourceDesc.Alignment = 0;
+		resourceDesc.Height = 1;
+		resourceDesc.DepthOrArraySize = 1;
+		resourceDesc.MipLevels = 1;
+		resourceDesc.Format = DXGI_FORMAT_UNKNOWN;
+		resourceDesc.SampleDesc.Count = 1;
+		resourceDesc.SampleDesc.Quality = 0;
+		resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+		resourceDesc.Width = requestedSize;
+		resourceDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+
+		ID3D12Resource* pool = nullptr;
+		globals::gDevice->CreateCommittedResource(&heapProps, D3D12_HEAP_FLAG_NONE, &resourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&pool));
 	}
 
-	void GraphicsMemoryManager::Destroy() noexcept
+	void GpuMemoryManager::Destroy() noexcept
 	{
-		if (_uploadBuffer != nullptr)
-			_uploadBuffer->Release();
-		if (_texturePool != nullptr)
-			_texturePool->Release();
-		if (_bufferPool != nullptr)
-			_bufferPool->Release();
+		for (size_t i = 0; i < HEAP_TYPES_COUNT; ++i)
+			for (size_t j = 0; j < HEAP_RESOURCES_TYPES_COUNT; ++j)
+				for (size_t k = 0; k < RESOURCE_TYPES_COUNT; ++k)
+					_pools[i][j][k].clear();
 	}
 
 }
--- source/engine/modules/renderer_core/resources/graphics_memory_manager.hpp
+++ source/engine/modules/renderer_core/resources/graphics_memory_manager.hpp
@@ -1,45 +1,76 @@
 #pragma once 
 
 #include <core/core.hpp>
+#include <core/lib/array.hpp>
 #include <core/debug/assert.hpp>
 #include <d3d12.h>
 
+#define HEAP_TYPES_COUNT 0x3U
+#define RESOURCE_TYPES_COUNT 0x2U
+#define HEAP_RESOURCES_TYPES_COUNT 0x3U
+
+#define HEAP_TYPE_MASK 0xC000000000000000
+#define HEAP_TYPE_DEFAULT 0x0U
+#define HEAP_TYPE_UPLOAD 0x1U
+#define HEAP_TYPE_READBACK 0x2U
+#define HEAP_TYPE_SHIFT 0x3EU
+
+#define RESOURCE_TYPE_MASK 0x3000000000000000
+#define RESOURCE_TYPE_COMMITTED 0x0U
+#define RESOURCE_TYPE_PLACED 0x1U
+#define RESOURCE_TYPE_SHIFT 0x3CU
+
+#define HEAP_RESOURCES_TYPE 0x0C00000000000000
+#define HEAP_RESOURCES_TYPE_BUFFER 0x0U
+#define HEAP_RESOURCES_TYPE_TEXTURE 0x1U
+#define HEAP_RESOURCES_TYPE_RTV_AND_DSV 0x2U
+#define HEAP_RESOURCES_TYPE_SHIFT 0x3AU
+
 namespace ray::renderer_core_api::resources
 {
-	class GraphicsMemoryManager
+	struct GpuMemoryPool
+	{
+		~GpuMemoryPool()
+		{
+			AvailableSpace = 0;
+			Committed.Pool->Release();
+		}
+
+		union
+		{
+			struct
+			{
+				ID3D12Resource* Pool;
+			} Committed;
+
+			struct
+			{
+				ID3D12Heap* Pool;
+			} Placed;
+		};
+
+		u64 AvailableSpace;
+
+	} ;
+
+	class GpuMemoryManager
 	{
 	public:
-		void Initialize(size_t maxTexturePoolSize = MB(128), size_t maxBufferPoolSize = MB(128)) noexcept;
+		~GpuMemoryManager()
+		{
+			Destroy();
+		}
 
-		void* AllocateFromUploadBuffer(size_t requestedSize) noexcept;
-		void AllocateFromTexturePool() noexcept {}
-		void AllocateFromBufferPool() noexcept {}
+		GpuMemoryPool& RequestPool(u64 requestedSize, u16 heapType, u16 heapResourcesType, u16 resourceType) noexcept;
 
+	private:
+		GpuMemoryPool& CreateNewPool(u64 requestedSize, u16 heapType, u16 heapResourcesType, u16 resourceType) noexcept;
+		ID3D12Heap* CreateHeap(u64 requestedSize, u16 heapType, u16 heapResourcesType) noexcept;
+		ID3D12Resource* CreateCommittedPool(u64 requestedSize, u16 heapType, u16 heapResourcesType) noexcept;
 		void Destroy() noexcept;
 
 	private:
-		void CreateUploadBuffer() noexcept;
-		void CreateTexturePool(size_t maxTexturePoolSize) noexcept;
-		void CreateBufferPool(size_t maxBufferPoolSize) noexcept;
-
-	private:
-		ID3D12Resource* _uploadBuffer; // ResourcesLoader and RingBuffer use this buffer
-		ID3D12Resource* _texturePool; // may be ID3D12Heap?
-		ID3D12Resource* _bufferPool; // may be ID3D12Heap?
-		
-		/*	
-			256 MB for ResourcesLoader, 
-			64 MB for RingBuffer and 
-			8 MB reserved 
-		*/
-
-		void* _beginOfUploadBuffer;
-		u8*	  _currentPointerOfUploadBuffer;
-		void* _endOfUploadBuffer;
-
-		const size_t MAX_UPLOAD_BUFFER_SIZE = MB(256 + 64 + 8); 
-		//size_t _maxTexturePoolSize; // determined in run-time
-		//size_t _maxBufferPoolSize; // determined in run-time
+		Array<GpuMemoryPool> _pools[HEAP_TYPES_COUNT][HEAP_RESOURCES_TYPES_COUNT][RESOURCE_TYPES_COUNT];
 
 	};
 }
